c6ec41f355b4efdde2567769021050b42d2823e6
Matteocapo12 <dellorsomatteo88@gmail.com>
2026-03-01 19:37:30 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "super-secret-gym-key";
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
